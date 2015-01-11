5176d72e5f9b01d692b15e210456354ac4e412c3
Nanang Mahdaen El Agung <mahdaen@gmail.com>
2015-01-11 22:19:26 +0700

    Updating docs

--- api_project.js
+++ api_project.js
@@ -1,13 +1,13 @@
 define({
   "name": "DOMList",
-  "version": "1.0.0",
-  "description": "A small `document.querySelectorAll()` helper.<br/>Extend the functionallity of native selector.<br/>Use it if you don't want to use jQuery and targetting to modern browsers.",
+  "version": "1.1.0",
+  "description": "A small `document.querySelectorAll()` helper.<br/>Extend the functionallity of native selector.<br/>Use it if you don't want to use jQuery and targetting to modern browsers. Included NativeJS.",
   "title": "DOMList - Native Query Helper",
   "sampleUrl": false,
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-01-10T18:45:20.669Z",
+    "time": "2015-01-11T15:16:00.089Z",
     "url": "http://apidocjs.com",
     "version": "0.12.0"
   }
