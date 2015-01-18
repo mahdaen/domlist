fbfd8eed52d64d9d6c78d9edeb650600a8942b10
Nanang Mahdaen El Agung <mahdaen@gmail.com>
2015-01-19 04:34:15 +0700

    Updating docs

--- api_project.js
+++ api_project.js
@@ -1,13 +1,13 @@
 define({
   "name": "DOMList",
-  "version": "2.0.0",
+  "version": "2.1.0",
   "description": "A small `document.querySelectorAll()` helper.<br/>Extend the functionallity of native selector.<br/>Use it if you don't want to use jQuery and targetting to modern browsers.<br>Included NativeJS.<br>If you want to use DOMList in old browser, you can load Sizzle before loading DOMList.",
   "title": "DOMList - Native Query Helper",
   "sampleUrl": false,
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-01-18T14:39:32.948Z",
+    "time": "2015-01-18T21:28:08.284Z",
     "url": "http://apidocjs.com",
     "version": "0.12.0"
   }
